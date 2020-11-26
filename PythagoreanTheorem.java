d0658d01b362e1f13ad92158f8560b64280f4634
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-11-25 18:47:38 -0800

    Fixed typo (there was no ';')

--- PythagoreanTheorem.java
+++ PythagoreanTheorem.java
@@ -1,8 +1,8 @@
 import java.lang.math;
 public PythagoreanTheorem {
-    public double calculateHypotenuse(int sideA, int sideB)
-    double sideA = Math.sqrt(sideA)
-    double sideB = Math.sqrt(sideB)
-    int sideC = sideA + sideB
-    return sideC
+    public double calculateHypotenuse(int sideA, int sideB);
+    double sideA = Math.sqrt(sideA);
+    double sideB = Math.sqrt(sideB);
+    int sideC = sideA + sideB;
+    return sideC;
 }
