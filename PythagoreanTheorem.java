3a369aad4b128070a259e8d90f6010c875cb1c96
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-11-25 18:54:32 -0800

    Fixed typo (forgot '{' and '}' for calculateHypotenuse class)

--- PythagoreanTheorem.java
+++ PythagoreanTheorem.java
@@ -1,8 +1,9 @@
 import java.lang.Math;
 public PythagoreanTheorem {
-    public double calculateHypotenuse(int sideA, int sideB);
-    double sideA = Math.sqrt(sideA);
-    double sideB = Math.sqrt(sideB);
-    int sideC = sideA + sideB;
-    return sideC;
+    public double calculateHypotenuse(int sideA, int sideB) {
+        double sideA = Math.sqrt(sideA);
+        double sideB = Math.sqrt(sideB);
+        int sideC = sideA + sideB;
+        return sideC;
+    }
 }
